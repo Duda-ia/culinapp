ce69be2b0ac6c3d778dbcae7becd44c2adfe8704
beatrizbraga <biabbraga@gmail.com>
2025-03-24 22:54:53 -0300

    style game

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -9,22 +9,27 @@ class QuizzesController < ApplicationController
     @selected_answer = @question.quiz_answers.find_by(id: params[:quiz_answer_id])
     @correct_answer = @question.quiz_answers.find_by(correct: true)
 
-    # Check correctness
+    # Always store the selected answer
+    session[:selected_answer] = @selected_answer&.content
+    session[:correct_answer] = @correct_answer.content
+    session[:quiz_question_id] = @question.id
+
+
     if @selected_answer == @correct_answer
       session[:quiz_result] = "correct"
     else
       session[:quiz_result] = "incorrect"
-      session[:selected_answer] = @selected_answer&.content
-      session[:correct_answer] = @correct_answer.content
     end
+
     redirect_to quiz_score_path(return_to: params[:return_to])
   end
-
 
   def score
     @result = session.delete(:quiz_result)
     @selected_answer = session.delete(:selected_answer)
     @correct_answer = session.delete(:correct_answer)
     @return_to = params[:return_to]
+    @question = QuizQuestion.find_by(id: session.delete(:quiz_question_id))
   end
+
 end
